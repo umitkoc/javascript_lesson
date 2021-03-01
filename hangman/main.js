73be7e02be5cbefce51734a722dd9a5516ea82f9
umit koc <umitkoc.com@gmail.com>
2021-03-02 01:41:29 +0300

    add music to handman

--- hangman/main.js
+++ hangman/main.js
@@ -1,73 +1,133 @@
-const ul=document.getElementById('ul')
-const wordlist=document.getElementById('word')
-const list=wordlist.childNodes
-const items=document.querySelectorAll('.item')
-var step=0
-var data=[
-    'java','python','javascript','dart'
-]
-var copyword=[]
-const letters=['a','b','c','d','e','f','g','i','ı','j','k','l','m','n','o','ö','p','r','s','ş','t','u','ğ','h','ü','v','y','z','w','q']
-const keys=[]
-function Random(){
-    return data[Math.floor(Math.random()*data.length)]
+const ul = document.getElementById("ul");
+const wordlist = document.getElementById("word");
+const items = document.querySelectorAll(".item");
+const card = document.getElementById("card");
+const list = wordlist.childNodes;
+var point = 0;
+var step = 0;
+var start = false;
+var data = [
+  "announce",
+  "always",
+  "javascript",
+  "programmer",
+  "afternoon",
+  "airport",
+];
+var copyword = [];
+const letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "i",
+  "ı",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "ö",
+  "p",
+  "r",
+  "s",
+  "ş",
+  "t",
+  "u",
+  "ğ",
+  "h",
+  "ü",
+  "v",
+  "y",
+  "z",
+  "w",
+  "q",
+];
+var keys = [];
+function Random() {
+  return data[Math.floor(Math.random() * data.length)];
 }
-function Start(){
-    let word=Random()
-    copyword=word=word.split('')
-    word.forEach(element => {
-        var li=document.createElement('li')
-        if(word[word.length-1]==element || word[word.length-3]==element ){
-             li.innerText=element
-        }else{
-            li.innerHTML='&nbsp'
-        }
-        wordlist.appendChild(li)
-    });
-    items.forEach(item => {
-        item.style.display='none'
-    });
+function Start() {
+  console.log("start çalıştı " + start);
+  step = 0;
+  keys = [];
+  copyword = [];
+  let word = Random();
+  data.push(word);
+  copyword = word = word.split("");
+  ul.innerHTML = "";
+  wordlist.innerHTML = "";
+  word.forEach((element) => {
+    var li = document.createElement("li");
+    if (word[word.length - 1] == element || word[word.length - 3] == element) {
+      li.innerText = element;
+    } else {
+      li.innerHTML = "&nbsp";
+    }
+    wordlist.appendChild(li);
+  });
+  items.forEach((item) => {
+    item.style.display = "none";
+  });
 }
 
-
- function Answer(event){
-    const next=keys.filter(i=>i==event.key)
-    const letter=letters.filter(i=>i==event.key)
-    if(next.length==0 && letter.length==1){
-        keys.push(event.key)
-        var li=document.createElement('li')
-        li.innerText=event.key
-        ul.appendChild(li)
-        Analysis(event.key)
+function Answer(event) {
+  if (start) {
+    const next = keys.filter((i) => i == event.key);
+    const letter = letters.filter((i) => i == event.key);
+    if (next.length == 0 && letter.length == 1) {
+      keys.push(event.key);
+      var li = document.createElement("li");
+      li.innerText = event.key;
+      ul.appendChild(li);
+      Analysis(event.key);
     }
+  } else {
+    start = true;
+    card.style.display = "none";
+    document.getElementById("music").play();
+    document.getElementById("point").innerText = 0;
+    Start();
+  }
 }
 
-function Analysis(key){
-    var dead=true
-    var win=2
-    for(var i=0;i<list.length-1;i++){
-        if(copyword[i]==key){
-            list[i+1].innerHTML=key
-            dead=false
-            break
-        }
-        if(list[i+1].innerText!=''){
-            win++
-        }
-        
-        
+function Analysis(key) {
+  var dead = true;
+  var win = 0;
+  for (var i = 1; i < list.length; i++) {
+    if (copyword[i - 1] == key) {
+      list[i - 1].innerHTML = key;
+      dead = false;
     }
-    if(dead){
-        items[step].style.display='block'
-        step++
-        if(step==10){
-            console.log('kaybettiniz')
-        }
+    if (list[i].innerHTML != "&nbsp;") {
+      win += 1;
     }
-    if(copyword.length==win){
-        console.log('kazandınız')
+  }
+  if (dead) {
+    items[step].style.display = "block";
+    step += 1;
+    console.log(step);
+    if (step == items.length) {
+      start = false;
+      card.style.display = "flex";
+      document.getElementById("music").pause();
+      document.getElementById("music").currentTime = 0;
     }
+  }
+  if (copyword.length == win + 2) {
+    ul.innerHTML = "";
+    point += list.length * 5;
+    document.getElementById("point").innerText = point;
+    Start();
+  }
+  if (data.length == 0) {
+    start = false;
+    card.style.display = "flex";
+    document.getElementById("music").pause();
+    document.getElementById("music").currentTime = 0;
+  }
 }
-
-Start()
-
